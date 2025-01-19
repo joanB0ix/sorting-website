056f48c29b57d8c4753f69ab046cba359bafa1b2
joanB0ix <joanboixavalos@gmail.com>
2025-01-19 17:58:34 +0100

    Add fonts and styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,6 +12,9 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        "fira-code": ["var(--font-fira-code)"],
       },
     },
   },
